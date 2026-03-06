2e02db47905070ea1678972bd6e4b742a4f350ca
koppepan3 <64785351+koppepan3@users.noreply.github.com>
2026-03-06 17:46:16 +0900

    feat: Astroの設定を更新し、PartytownとSitemapの統合を追加
    - astro.config.mjs: PartytownとSitemapの統合を追加し、サイト情報を設定
    - package.json: PartytownとSitemapの依存関係を追加
    - package-lock.json: PartytownとSitemapのバージョンを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,32 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+
+import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
+
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://wakabasai.tpgd.jp',
+
+  vite: {
+      resolve: {
+          alias: {
+              '@layouts': '/src/layouts',
+              '@components': '/src/components',
+              '@lib': '/src/lib',
+              '@assets': '/src/assets',
+          },
+      },
+  },
+
+  integrations: [
+    sitemap(),
+    partytown({
+      config: {
+        forward: [["dataLayer.push"],"gtm.push"],
+      },
+    })
+  ],
+});
